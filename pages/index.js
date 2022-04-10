d9b4434cc5cd2b31dda75c9c543954c6dd715229
polyalya <98054317+polyalya@users.noreply.github.com>
2022-04-10 14:07:16 +0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById(".profile__edit-button").addEventListener("click", function(){
-    document.querySelector(".popup-modal").style.display = "flex";
-})
-
-document.querySelector(".popup-modal__close").addEventListener("click", function(){
-    document.querySelector(".popup-modal").style.display = "none";
-})
